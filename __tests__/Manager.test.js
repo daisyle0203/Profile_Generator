06ca314132f1a6da5110387ba2ed990056cc8bc3
Daisy Le <daisy.le0203@gmail.com>
2022-10-07 14:48:31 -0500

    added test cases for Manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,46 +1,50 @@
-const Manager = require('../lib/Manager')
+const Manager = require("../lib/Manager")
 const empName = "Alex"
 const id = 2
 const email = "alex@email.com"
 const officeNum = 123
-const testManager = new Manager(empName,id,email,officeNum)
+const testManager = new Manager(empName, id, email, officeNum)
 
-
-test("We can create a new instantiation of the Manager class", ()=>{
-    const testValue = testManager
-    expect(typeof(testValue)).toBe("object")
+test("We can create a new instantiation of the Manager class", () => {
+  const testValue = testManager
+  expect(typeof(testValue)).toBe("object")
 })
 
-test("Class manager has name attribute", ()=>{
-    const testValue = "Alex"
-    expect(testManager.name).toBe(testValue)
+test("Class manager has name attribute", () => {
+  const testValue = "Alex"
+  expect(testManager.name).toBe(testValue)
 })
 
-test("Class manager has id attribute", ()=>{
-    const testValue = 2
-    expect(testManager.id).toBe(testValue)
+test("Class manager has id attribute", () => {
+  const testValue = 2
+  expect(testManager.id).toBe(testValue)
 })
 
-test("Class manager has email attribute", ()=>{
-    const testValue = "alex@email.com"
-    expect(testManager.email).toBe(testValue)
+test("Class manager has email attribute", () => {
+  const testValue = "alex@email.com"
+  expect(testManager.email).toBe(testValue)
+})
+test("Class manager has office attribute", () => {
+  const testValue = 123
+  expect(testManager.officeNum).toBe(testValue)
 })
 
-test("Class manager has getName method that returns the name", ()=>{
-    const testValue = empName
-    expect(testManager.getName()).toBe(testValue)
+test("Class manager has getName method that returns the name", () => {
+  const testValue = empName
+  expect(testManager.getName()).toBe(testValue)
 })
 
-test("Class manager has getId method that returns the id", ()=>{
-    const testValue = id
-    expect(testManager.getId()).toBe(testValue)
+test("Class manager has getId method that returns the id", () => {
+  const testValue = id
+  expect(testManager.getId()).toBe(testValue)
 })
 
-test("Class manager has getEmail method that returns the email", ()=>{
-    const testValue = email
-    expect(testManager.getEmail()).toBe(testValue)
+test("Class manager has getEmail method that returns the email", () => {
+  const testValue = email
+  expect(testManager.getEmail()).toBe(testValue)
 })
-test("Class manager has getRole method that returns the role", ()=>{
-    const testValue = "Manager"
-    expect(testManager.getRole()).toBe(testValue)
+
+test("Class manager has getRole method that returns the role", () => {
+  const testValue = "Manager"
+  expect(testManager.getRole()).toBe(testValue)
 })
